5080cb9add70a29ddc3539f410bfc708ff20ac76
Christian Diaz <rivalch2015@gmail.com>
2026-08-23 15:17:54 -0400

    Cambios en prisma

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,5 +1,5 @@
 import "dotenv/config";
-import { PrismaClient } from "../generated/prisma/client.ts";
+import { PrismaClient } from "@prisma/client";
 import bcrypt from "bcrypt";
 
 const prisma = new PrismaClient();
